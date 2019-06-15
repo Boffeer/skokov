310fdee2d297b27895f2f8a0006d5c2f1741c000
Nick <boffeechane@gmail.com>
2019-06-15 20:46:08 +0300

    road to ipad

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     svg4everybody({});
 });
+
+$(document).ready(function () {
+	$(window).on("scroll", function () {
+		if($(window).scrollTop() > 100) {
+					$(".header").addClass('header--active');
+	} else {
+			$(".header").removeClass('header--active');
+		}
+	})
+});
