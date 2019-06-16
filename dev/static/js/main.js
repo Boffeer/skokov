cabd8a6837be09f76f121db1cc4112f7890c3038
Nick <boffeechane@gmail.com>
2019-06-16 15:24:37 +0300

    cosmetical

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     svg4everybody({});
 });
 
+
+// dark header-bg
 $(document).ready(function () {
 	$(window).on("scroll", function () {
 		if($(window).scrollTop() > 100) {
@@ -10,4 +12,13 @@ $(document).ready(function () {
 			$(".header").removeClass('header--active');
 		}
 	})
+});
+
+
+$(document).ready(function () {
+	$('.clients-slider').slick({
+		dots: true,
+		infinite:true,
+		speed: 500
+	});
 });
