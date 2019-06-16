e669f0391d15eef29fe0255a29bb8aed5c7924a4
Nick <boffeechane@gmail.com>
2019-06-16 17:15:40 +0300

    burger

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -14,11 +14,53 @@ $(document).ready(function () {
 	})
 });
 
+// burger
+$(document).ready(function () {
+	$('.burger').click(function () {
+		$(this).toggleClass('burger--active');
+		$('.nav-menu').toggleClass('nav-menu--active')
+		if($(window).scrollTop() > 100) {
+		} else {
+			$('.header').addClass('header--active')	
+		}
+		
+	});
+});
 
+// clients slider
 $(document).ready(function () {
 	$('.clients-slider').slick({
 		dots: true,
 		infinite:true,
 		speed: 500
+	});
+});
+
+// our team slider
+$(document).ready(function () {
+	$('.our-team__slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 	});
 });
